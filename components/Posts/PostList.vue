f952d8aa50ba799505c9cf3aebeb2cfdff48ee30
Hugo Rodrigues <hugo.rodrigues@codedesign.org>
2018-03-29 15:29:34 +0100

    Initial Commit

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,46 @@
+<template>
+    <section class="post-list">
+        <PostPreview
+            id="1"
+            thumbnail="https://placeimg.com/640/480/tech"
+            title="Hello there"
+            previewText="This is a post"
+        />
+        <PostPreview
+            id="2"
+            thumbnail="https://placeimg.com/640/480/tech"
+            title="Hello there"
+            previewText="This is a post"
+        />
+        <PostPreview
+            id="3"
+            thumbnail="https://placeimg.com/640/480/tech"
+            title="Hello there"
+            previewText="This is a post"
+        />
+    </section>
+</template>
+
+<script>
+    import PostPreview from '@/components/Posts/PostPreview';
+
+    export default {
+        name: 'PostList',
+        components: {
+            PostPreview
+        }
+    }
+</script>
+
+<style scoped>
+    .post-list {
+        display: flex;
+        padding: 20px;
+        box-sizing: border-box;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
+
+
